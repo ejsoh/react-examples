56376aaf69d5479315ecfb1664f2954adaabc987
megamisoh <rekagirl@naver.com>
2022-01-19 10:35:43 +0900

    couter examples;

--- src/pages/Second.tsx
+++ src/pages/Second.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Second = () => {
-  return <div>second</div>;
+  // state setting
+  const [state, setState] = React.useState(0);
+
+  // increase function
+  const increase = () => setState(state + 1);
+
+  return (
+    <div className="counter-container">
+      <h1>counter examples</h1>
+      <div className="counter-items">
+        <button className="counter-reset">reset</button>
+        <button>-</button>
+        {state}
+        <button onClick={increase}>+</button>
+      </div>
+    </div>
+  );
 };
 
 export default Second;
